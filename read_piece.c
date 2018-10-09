fd4404eaa14b38e141eeba045424c704d51dd97f
Yunus ABDULHAYOGLU <yunusemin-github@yahoo.com>
2018-10-09 11:38:00 +0200

    Added makefile

---
+++ read_piece.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   read_piece.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yabdulha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/09 11:01:55 by yabdulha          #+#    #+#             */
+/*   Updated: 2018/10/09 11:33:50 by yabdulha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "filler.h"
+
+/*
+
+int		read_piece(t_filler *data, char *line)
+{
+	char	*piece;
+	int		x;
+	int		y;
+
+	x = ft_atoi(ft_strchr(line, ' '));
+	y = ft_atoi(ft_strrchr(line, ' '));
+	piece = (char*)malloc(sizeof(*piece) * );
+	while (get_next_line(0, &line))
+	{
+
+	}
+}
+
+*/
+
+
